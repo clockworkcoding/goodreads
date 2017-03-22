4642b1e15c6e7eb9a460e881229eef7f1087142c
Max Meltser <Max Meltser>
2017-03-22 01:13:44 -0500

    removed pointers from structs

--- goodreads.go
+++ goodreads.go
@@ -13,14 +13,14 @@ var (
 )
 
 type Search_GoodreadsResponse struct {
-	Search_Request *Search_Request `xml:" Request,omitempty" json:"Request,omitempty"`
-	Search_search  *Search_search  `xml:" search,omitempty" json:"search,omitempty"`
+	Search_Request Search_Request `xml:" Request,omitempty" json:"Request,omitempty"`
+	Search_search  Search_search  `xml:" search,omitempty" json:"search,omitempty"`
 }
 
 type Search_Request struct {
-	Search_authentication *Search_authentication `xml:" authentication,omitempty" json:"authentication,omitempty"`
-	Search_key            *Search_key            `xml:" key,omitempty" json:"key,omitempty"`
-	Search_method         *Search_method         `xml:" method,omitempty" json:"method,omitempty"`
+	Search_authentication Search_authentication `xml:" authentication,omitempty" json:"authentication,omitempty"`
+	Search_key            Search_key            `xml:" key,omitempty" json:"key,omitempty"`
+	Search_method         Search_method         `xml:" method,omitempty" json:"method,omitempty"`
 }
 
 type Search_authentication struct {
@@ -28,8 +28,8 @@ type Search_authentication struct {
 }
 
 type Search_author struct {
-	Search_id   *Search_id   `xml:" id,omitempty" json:"id,omitempty"`
-	Search_name *Search_name `xml:" name,omitempty" json:"name,omitempty"`
+	Search_id   Search_id   `xml:" id,omitempty" json:"id,omitempty"`
+	Search_name Search_name `xml:" name,omitempty" json:"name,omitempty"`
 }
 
 type Search_average_rating struct {
@@ -37,12 +37,12 @@ type Search_average_rating struct {
 }
 
 type Search_best_book struct {
-	Attr_type              string                  `xml:" type,attr"  json:",omitempty"`
-	Search_author          *Search_author          `xml:" author,omitempty" json:"author,omitempty"`
-	Search_id              *Search_id              `xml:" id,omitempty" json:"id,omitempty"`
-	Search_image_url       *Search_image_url       `xml:" image_url,omitempty" json:"image_url,omitempty"`
-	Search_small_image_url *Search_small_image_url `xml:" small_image_url,omitempty" json:"small_image_url,omitempty"`
-	Search_title           *Search_title           `xml:" title,omitempty" json:"title,omitempty"`
+	Attr_type              string                 `xml:" type,attr"  json:",omitempty"`
+	Search_author          Search_author          `xml:" author,omitempty" json:"author,omitempty"`
+	Search_id              Search_id              `xml:" id,omitempty" json:"id,omitempty"`
+	Search_image_url       Search_image_url       `xml:" image_url,omitempty" json:"image_url,omitempty"`
+	Search_small_image_url Search_small_image_url `xml:" small_image_url,omitempty" json:"small_image_url,omitempty"`
+	Search_title           Search_title           `xml:" title,omitempty" json:"title,omitempty"`
 }
 
 type Search_books_count struct {
@@ -102,7 +102,7 @@ type Search_ratings_count struct {
 }
 
 type Search_results struct {
-	Search_work []*Search_work `xml:" work,omitempty" json:"work,omitempty"`
+	Search_work []Search_work `xml:" work,omitempty" json:"work,omitempty"`
 }
 
 type Search_results_end struct {
@@ -114,17 +114,17 @@ type Search_results_start struct {
 }
 
 type Search_root struct {
-	Search_GoodreadsResponse *Search_GoodreadsResponse `xml:" GoodreadsResponse,omitempty" json:"GoodreadsResponse,omitempty"`
+	Search_GoodreadsResponse Search_GoodreadsResponse `xml:" GoodreadsResponse,omitempty" json:"GoodreadsResponse,omitempty"`
 }
 
 type Search_search struct {
-	Search_query              *Search_query              `xml:" query,omitempty" json:"query,omitempty"`
-	Search_query_time_seconds *Search_query_time_seconds `xml:" query-time-seconds,omitempty" json:"query-time-seconds,omitempty"`
-	Search_results            *Search_results            `xml:" results,omitempty" json:"results,omitempty"`
-	Search_results_end        *Search_results_end        `xml:" results-end,omitempty" json:"results-end,omitempty"`
-	Search_results_start      *Search_results_start      `xml:" results-start,omitempty" json:"results-start,omitempty"`
-	Search_source             *Search_source             `xml:" source,omitempty" json:"source,omitempty"`
-	Search_total_results      *Search_total_results      `xml:" total-results,omitempty" json:"total-results,omitempty"`
+	Search_query              Search_query              `xml:" query,omitempty" json:"query,omitempty"`
+	Search_query_time_seconds Search_query_time_seconds `xml:" query-time-seconds,omitempty" json:"query-time-seconds,omitempty"`
+	Search_results            Search_results            `xml:" results,omitempty" json:"results,omitempty"`
+	Search_results_end        Search_results_end        `xml:" results-end,omitempty" json:"results-end,omitempty"`
+	Search_results_start      Search_results_start      `xml:" results-start,omitempty" json:"results-start,omitempty"`
+	Search_source             Search_source             `xml:" source,omitempty" json:"source,omitempty"`
+	Search_total_results      Search_total_results      `xml:" total-results,omitempty" json:"total-results,omitempty"`
 }
 
 type Search_small_image_url struct {
@@ -149,15 +149,15 @@ type Search_total_results struct {
 }
 
 type Search_work struct {
-	Search_average_rating             *Search_average_rating             `xml:" average_rating,omitempty" json:"average_rating,omitempty"`
-	Search_best_book                  *Search_best_book                  `xml:" best_book,omitempty" json:"best_book,omitempty"`
-	Search_books_count                *Search_books_count                `xml:" books_count,omitempty" json:"books_count,omitempty"`
-	Search_id                         *Search_id                         `xml:" id,omitempty" json:"id,omitempty"`
-	Search_original_publication_day   *Search_original_publication_day   `xml:" original_publication_day,omitempty" json:"original_publication_day,omitempty"`
-	Search_original_publication_month *Search_original_publication_month `xml:" original_publication_month,omitempty" json:"original_publication_month,omitempty"`
-	Search_original_publication_year  *Search_original_publication_year  `xml:" original_publication_year,omitempty" json:"original_publication_year,omitempty"`
-	Search_ratings_count              *Search_ratings_count              `xml:" ratings_count,omitempty" json:"ratings_count,omitempty"`
-	Search_text_reviews_count         *Search_text_reviews_count         `xml:" text_reviews_count,omitempty" json:"text_reviews_count,omitempty"`
+	Search_average_rating             Search_average_rating             `xml:" average_rating,omitempty" json:"average_rating,omitempty"`
+	Search_best_book                  Search_best_book                  `xml:" best_book,omitempty" json:"best_book,omitempty"`
+	Search_books_count                Search_books_count                `xml:" books_count,omitempty" json:"books_count,omitempty"`
+	Search_id                         Search_id                         `xml:" id,omitempty" json:"id,omitempty"`
+	Search_original_publication_day   Search_original_publication_day   `xml:" original_publication_day,omitempty" json:"original_publication_day,omitempty"`
+	Search_original_publication_month Search_original_publication_month `xml:" original_publication_month,omitempty" json:"original_publication_month,omitempty"`
+	Search_original_publication_year  Search_original_publication_year  `xml:" original_publication_year,omitempty" json:"original_publication_year,omitempty"`
+	Search_ratings_count              Search_ratings_count              `xml:" ratings_count,omitempty" json:"ratings_count,omitempty"`
+	Search_text_reviews_count         Search_text_reviews_count         `xml:" text_reviews_count,omitempty" json:"text_reviews_count,omitempty"`
 }
 
 func GetSearch(query, key string) Search_GoodreadsResponse {
