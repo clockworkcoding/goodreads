73844d1367951fb4ac67a78ac2aba1452b2cda30
clockworkcoding <yodadamax@gmail.com>
2017-04-03 00:25:15 -0500

    added shelf methods

--- user_test.go
+++ user_test.go
@@ -15,3 +15,12 @@ func TestGetUser(t *testing.T) {
 		fmt.Println(xml)
 	}
 }
+
+func TestAddToShelf(t *testing.T) {
+	config := readConfig()
+	c := NewClientWithToken(config.GoodReadsKey, config.GoodReadsSecret, config.SlackToken, config.GoodReadsHost)
+	if err := c.AddBookToShelf("30078567", "read-next"); err != nil {
+		fmt.Println(err.Error())
+		t.Fail()
+	}
+}
